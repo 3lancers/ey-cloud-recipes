41b4b55520ceb78d8607033fac0aa7d2397ae77e
Tomas Cigan <tomas@cigan.cz>
2010-11-30 18:03:08 +0100

    uprava

--- cookbooks/whenever_crontab/recipes/default.rb
+++ cookbooks/whenever_crontab/recipes/default.rb
@@ -3,7 +3,7 @@ node[:applications].each do |app,data|
 execute "run whenever script" do
     user node[:owner_name]
     group node[:owner_name]
-    command("if [ -e /data/#{app}/current/schedule.rb ] ; " +
+    command("if [ -e /data/#{app}/current/config/schedule.rb ] ; " +
             "then cd /data/#{app}/current && " +
             "whenever --update-crontab '#{app}' --set environment=production; " +
             "fi")
